1affa918129c5d333c86fb4c84aea2b7d616ecdf
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2023-01-18 20:28:20 -0800

    tryed to fix the recalibrate button

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,62 +7,66 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.subsystems.Drivetrain;
+import frc.robot.commands.ResetSwerveGyroCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.SwerveModule;
 
 public class Robot extends TimedRobot {
   public static Joystick rightJoystick = new Joystick(Constants.RIGHT_JOYSTICK_CHANNEL);
   public static Joystick leftJoystick = new Joystick(Constants.LEFT_JOYSTICK_CHANNEL);
-  
-  private final Drivetrain m_swerve = new Drivetrain();
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
   private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
-  private static Trigger recalibrateButton = new JoystickButton(rightJoystick, Constants.CALIBRATE_BUTTON);
+  private static Trigger recalibrateButton = new JoystickButton(rightJoystick, 3);
+
+  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+
+  private final ResetSwerveGyroCommand m_resetSwerveGyroCommand = new ResetSwerveGyroCommand(m_drivetrainSubsystem);
 
   public Robot() {
-    configureButtonBindings();
+    recalibrateButton.onTrue(m_resetSwerveGyroCommand);
   }
 
   private void configureButtonBindings() {
-    recalibrateButton.onTrue(m_swerve.recalibrateGyro());
+    
   }
 
   @Override
   public void autonomousPeriodic() {
     driveWithJoystick(false);
-    m_swerve.updateOdometry();
+    m_drivetrainSubsystem.updateOdometry();
   }
 
   @Override
   public void teleopPeriodic() {
-    m_swerve.m_frontLeft.updateSwerveTable(); // 0 analog ID 
-    m_swerve.m_frontRight.updateSwerveTable(); // 3 analog ID
-    m_swerve.m_backLeft.updateSwerveTable(); // 1 analog ID
-    m_swerve.m_backRight.updateSwerveTable(); //2 analog ID
+    m_drivetrainSubsystem.m_frontLeft.updateSwerveTable(); // 0 analog ID 
+    m_drivetrainSubsystem.m_frontRight.updateSwerveTable(); // 3 analog ID
+    m_drivetrainSubsystem.m_backLeft.updateSwerveTable(); // 1 analog ID
+    m_drivetrainSubsystem.m_backRight.updateSwerveTable(); //2 analog ID
     driveWithJoystick(true);
-    System.out.println("front left table ang: " + m_swerve.m_frontLeft.t_turningEncoder.getDouble(-1));
-    System.out.println("front right table ang: " + m_swerve.m_frontRight.t_turningEncoder.getDouble(-1));
-    System.out.println("back left table ang: " + m_swerve.m_backLeft.t_turningEncoder.getDouble(-1));
-    System.out.println("back right table ang: " + m_swerve.m_backRight.t_turningEncoder.getDouble(-1));
+    // System.out.println("front left table ang: " + m_drivetrainSubsystem.m_frontLeft.t_turningEncoder.getDouble(-1));
+    // System.out.println("front right table ang: " + m_drivetrainSubsystem.m_frontRight.t_turningEncoder.getDouble(-1));
+    // System.out.println("back left table ang: " + m_drivetrainSubsystem.m_backLeft.t_turningEncoder.getDouble(-1));
+    // System.out.println("back right table ang: " + m_drivetrainSubsystem.m_backRight.t_turningEncoder.getDouble(-1));
   }
 
   public void testPeriodic() {
-    m_swerve.m_frontLeft.updateSwerveTable(); // 0 analog ID 
-    m_swerve.m_frontRight.updateSwerveTable(); // 3 analog ID
-    m_swerve.m_backLeft.updateSwerveTable(); // 1 analog ID
-    m_swerve.m_backRight.updateSwerveTable(); //2 analog ID
-    System.out.println("front left table ang: " + m_swerve.m_frontLeft.t_turningEncoder.getDouble(-1));
-    System.out.println("front right table ang: " + m_swerve.m_frontRight.t_turningEncoder.getDouble(-1));
-    System.out.println("back left table ang: " + m_swerve.m_backLeft.t_turningEncoder.getDouble(-1));
-    System.out.println("back right table ang: " + m_swerve.m_backRight.t_turningEncoder.getDouble(-1));
+    m_drivetrainSubsystem.m_frontLeft.updateSwerveTable(); // 0 analog ID 
+    m_drivetrainSubsystem.m_frontRight.updateSwerveTable(); // 3 analog ID
+    m_drivetrainSubsystem.m_backLeft.updateSwerveTable(); // 1 analog ID
+    m_drivetrainSubsystem.m_backRight.updateSwerveTable(); //2 analog ID
+    System.out.println("front left table ang: " + m_drivetrainSubsystem.m_frontLeft.t_turningEncoder.getDouble(-1));
+    System.out.println("front right table ang: " + m_drivetrainSubsystem.m_frontRight.t_turningEncoder.getDouble(-1));
+    System.out.println("back left table ang: " + m_drivetrainSubsystem.m_backLeft.t_turningEncoder.getDouble(-1));
+    System.out.println("back right table ang: " + m_drivetrainSubsystem.m_backRight.t_turningEncoder.getDouble(-1));
   }
 
   public void testInit() {
@@ -74,15 +78,15 @@ public class Robot extends TimedRobot {
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
     final var xSpeed =
-        m_xspeedLimiter.calculate(MathUtil.applyDeadband(rightJoystick.getY(), 0.02))
-            * Drivetrain.kMaxSpeed;
+        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(rightJoystick.getY(), 0.02))
+            * DrivetrainSubsystem.kMaxSpeed;
 
     // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
     final var ySpeed =
-        m_yspeedLimiter.calculate(MathUtil.applyDeadband(rightJoystick.getX(), 0.02))
-            * Drivetrain.kMaxSpeed;
+        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(rightJoystick.getX(), 0.02))
+            * DrivetrainSubsystem.kMaxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
@@ -91,8 +95,8 @@ public class Robot extends TimedRobot {
     final var rot =
     //must be positive to read accuate joystick yaw
         m_rotLimiter.calculate(MathUtil.applyDeadband(leftJoystick.getX(), 0.02))
-            * Drivetrain.kMaxAngularSpeed;
+            * DrivetrainSubsystem.kMaxAngularSpeed;
 
-    m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
+    m_drivetrainSubsystem.drive(xSpeed, ySpeed, rot, fieldRelative);
   }
 }
